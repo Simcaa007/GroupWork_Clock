aa328e4ef1349600a12969750da6ec7121158183
Simcaa007 <havelkova.si@seznam.cz>
2025-10-24 11:33:53 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-let nav = document.querySelector("nav");
-let tlacitko = document.querySelector(".kuk");
-
-tlacitko.addEventListener("click", function() {
-  nav.classList.toggle('nav--active');
-});
